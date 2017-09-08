082a48d1b12a98793c70a56503b1a5c5b2927e63
Paruyr Sahakyan <paruyrsahakyan@gmail.com>
2017-09-09 01:36:03 +0400

    delated remained comments

--- Number_4_1.java
+++ Number_4_1.java
@@ -19,14 +19,11 @@ boolean  ifNumbersAreFriendy(long a, long b){
 		    k=1;
 				for (int i=1; i<=aNumberOfDigits; i++){
 	                                       	a_dig=((a-(a%k))/k)%10;
-	                                       	//System.out.println("a_dig"+i+"="+ a_dig);
 	                                       	k=k*10;
 	                                      	result=false;
 	                                       	int c=1;
-	                                       //	b_current=b;
 	                                       	for (int j=1; j<=bNumberOfDigits; j++) {
 	                                       											b_dig=((b-(b%c))/c)%10;
-	                                       											//System.out.println("b_dig"+j+"="+ b_dig);
 	                                       											if (a_dig==b_dig) {System.out.println("a_dig"+i+"="+"b_dig"+j); result=true; break;}
 	                                       											else c=c*10;                                 }
 			                               	if (result==false)
